3186214bbd04660ab7a7af516ad924ffe3ef9eb1
Bhandavi19 <neelabhandavi@gmail.com>
2026-05-25 16:14:20 +0530

    Time: 1 ms (95.19%) | Memory: 43.3 MB (83.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,25 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> arr =new ArrayList<>();
-        
-        List<Integer> firstrow = new ArrayList<>();
-        firstrow.add(1);
-        arr.add(firstrow);
+        List<List<Integer>> l = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        l.add(row);
+
 
         for(int i=1;i<numRows;i++){
-            List<Integer> temprow =new ArrayList<>();
-            //in every array first element should be one 
-            temprow.add(1);
-                        
-            for(int j=1;j<i;j++){
-            int val=arr.get(i-1).get(j)+arr.get(i-1).get(j-1);
-                temprow.add(val);
-            }
-            //in every array last element should be one 
-            temprow.add(1);
-            arr.add(temprow);
-        }
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
 
-        return arr;
+            for(int j=1;j<i;j++){
+                int val = l.get(i-1).get(j-1) + l.get(i-1).get(j);
+                temp.add(val);
+            }
+
+            temp.add(1);
+            l.add(temp);
+        }
+        return l;
     }
 }
